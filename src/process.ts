8c6dc18a0801f4887fbc66dd29961043056e1d55
Lucas Muller <lucas.muller@sap.com>
2021-05-29 20:33:34 -0300

    Add process class

---
+++ src/process.ts
@@ -0,0 +1,22 @@
+import {Duration} from 'moment';
+
+export class Process {
+  private id = 0
+  private active = true
+
+  constructor(
+    private name: string,
+    private duration: Duration,
+  ) { }
+
+  getId = () => this.id;
+  getName = () => this.name;
+
+  getDuration = () => this.duration;
+  setDuration = (duration: Duration) => this.duration = duration;
+
+  isActive = () => this.active;
+  setActive = (active: boolean) => this.active = active;
+}
+
+export default Process;
