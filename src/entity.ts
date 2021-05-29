a87fb98323f92ed04abd436e858a8aa8e00c88e2
Lucas Muller <lucas.muller@sap.com>
2021-05-29 20:33:28 -0300

    Add entity class

---
+++ src/entity.ts
@@ -0,0 +1,26 @@
+import moment from 'moment';
+import PetriNet from './petri-net';
+
+export class Entity {
+  private id = 0
+  private priority = -1;
+  private creationTime = moment()
+
+  constructor(
+    private name: string,
+    private petriNet?: PetriNet,
+  ) { }
+
+  getId = () => this.id
+  getName = () => this.name
+
+  getPriority = () => this.priority
+  setPriority = (priority: number) => this.priority = priority
+
+  getPetriNet = () => this.petriNet
+  setPetriNet = (petriNet: PetriNet) => this.petriNet = petriNet
+
+  getTimeSinceCreation = () => moment().diff(this.creationTime)
+}
+
+export default Entity;
